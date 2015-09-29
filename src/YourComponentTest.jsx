66c7c0b2c31b336850601e1021f7559a5f830830
Davi Ferreira <contato@daviferreira.com>
2015-09-29 21:48:07 +0200

    Drop jsdom and use shallowRenderer on test example

--- src/YourComponentTest.jsx
+++ src/YourComponentTest.jsx
@@ -1,19 +1,18 @@
 'use strict';
 
 import expect from 'unexpected';
-import jsdom from 'mocha-jsdom';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 
 import YourComponent from './YourComponent';
 
+const shallowRenderer = TestUtils.createRenderer();
+
 describe('Your Component Test Case', () => {
 
-  jsdom();
-
   it('should render', () => {
-    var instance = TestUtils.renderIntoDocument(<YourComponent />);
-    expect(instance, 'to be defined');
+    shallowRenderer.render(<YourComponent />);
+    expect(shallowRenderer.getRenderOutput(), 'to be defined');
   });
 
 });
